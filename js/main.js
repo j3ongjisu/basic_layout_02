f6765e9b12bec256447594c1e7be07a67cd10aa5
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-06-08 14:46:58 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    //위로 올라가는 슬라이드
+    $('.main_slide').slick({
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+    })
+})
